1e24a8fe58e768337b84cd440df0b37a6f709337
Anass Fajoui <anasslpro165@gmail.com>
2025-04-02 16:04:07 +0000

    fixing something

--- src/App.tsx
+++ src/App.tsx
@@ -28,10 +28,6 @@ function App() {
 
     useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm]);
 
-    useEffect(()=>{
-        fetch("http://localhost:3000/").then(res => res.json()).then(data => console.log(data))
-    }, [])
-
     useEffect(() => {
         setMovies([]);
     }, [debouncedSearchTerm, selectedCategory]);
